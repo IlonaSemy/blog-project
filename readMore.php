8245e2b8999d151f42a325b014d4e4c5279f939b
Pol <pol292@gmail.com>
2017-04-18 00:24:30 +0300

    fix comment written by bug

--- readMore.php
+++ readMore.php
@@ -10,7 +10,7 @@ if ( !isset( $_SESSION[ 'user_id' ] ) && ($_SESSION[ 'user_name' ]) ) {
 $post_id  = filter_input( INPUT_GET, 'id', FILTER_VALIDATE_INT );
 $post_id  = trim( $post_id );
 $username = $_SESSION[ 'user_name' ];
-$comments=[];
+$comments = [];
 
 if ( $post_id ) {
 
@@ -20,18 +20,27 @@ if ( $post_id ) {
     $sql     = "SELECT p.*,u.name FROM posts p JOIN users u ON p.id='$post_id' AND p.user_id = u.id";
 
     $result = mysqli_query( $link, $sql );
-   /* if ( $result && mysqli_num_rows( $result ) == 1 ) { 
-        $post = mysqli_fetch_assoc( $result );*/ 
-    
-        if ( $result && mysqli_num_rows( $result ) == 1 ) {
-         $post = mysqli_fetch_assoc( $result );
-         $articleOneLine = htmlentities( $post[ 'article' ] );
-         $articleOneLine = explode( "\n", $articleOneLine );
-         $article = '';
-         foreach ( $articleOneLine as $line ) {
-                  $article .= $line . '<br>';
-         }
-        
+    /* if ( $result && mysqli_num_rows( $result ) == 1 ) { 
+      $post = mysqli_fetch_assoc( $result ); */
+
+    if ( $result && mysqli_num_rows( $result ) == 1 ) {
+        $post           = mysqli_fetch_assoc( $result );
+        $articleOneLine = htmlentities( $post[ 'article' ] );
+        $articleOneLine = explode( "\n", $articleOneLine );
+        $article        = '';
+        foreach ( $articleOneLine as $line ) {
+            $article .= $line . '<br>';
+        }
+
+        $comsql    = "SELECT c.*,u.name FROM comments c JOIN users u ON c.post_id = $post_id AND c.user_id = u.id ORDER BY commentdate DESC";
+        $comresult = mysqli_query( $link, $comsql );
+
+        if ( $comresult && mysqli_num_rows( $comresult ) > 0 ) {
+            while ( $comrow = mysqli_fetch_assoc( $comresult ) ) {
+
+                $comments[] = $comrow;
+            }
+        }
     } else {
 
         header( 'location: blog.php' );
@@ -39,22 +48,7 @@ if ( $post_id ) {
 } else {
 
     header( 'location: blog.php' );
-}  
-
-$com_link=mysqli_connect( MYSQL_HOST, MYSQL_USER, MYSQL_PWD, MYSQL_DB ); 
-$comsql="SELECT c.*,u.name FROM posts p JOIN users u ON p.user_id=u.id"
-        ." JOIN comments c on p.id=c.post_id ORDER BY commentdate DESC"; 
-$comresult = mysqli_query($com_link,$comsql); 
-
-if($comresult&& mysqli_num_rows($comresult)>0){ 
-    
-    while($comrow=mysqli_fetch_assoc($comresult)){ 
-        
-        $comments[]=$comrow; 
-       
-    }
-    
-} 
+}
 ?>
 
 
@@ -68,44 +62,44 @@ if($comresult&& mysqli_num_rows($comresult)>0){
                 <p><b>Written by:  </b><?= htmlentities( $post[ 'name' ] ); ?>, On:<?= $post[ 'date' ]; ?>
                 <h2><?= htmlentities( $post[ 'title' ] ); ?></h2>
                <!-- <p><? htmlentities( $post[ 'article' ] ); ?></p>  -->
-               <p><?= $article; ?></p>  
-               
-               </div> 
+                <p><?= $article; ?></p>  
+
+            </div> 
         </div> 
-  
+
     <?php endif; ?>   
-    
-    <input type="submit" name="submit" value="Leave a comment" onclick="window.location='add_comment.php?id=<?= $post['id']; ?>';"> <br><br>
-        
-   <!--<a href="add_comment.php?id" class="commentslink"> Leave a comment: </a><br><br> -->
-    
-       
-    <?php foreach ($comments as $comm): ?>  
-    
-    <?php if($comm['post_id']==$post['id']):?> 
-    <div class="sidebar-box-com"> 
-    <p><?= htmlentities($comm['comment']); ?></p>  
-    <p><b>Written by: </b><i><?= htmlentities($post['name']); ?></i>, On: <?= $comm['commentdate']; ?> 
-        <?php if ($comm['user_id']== $uid): ?>  
-    <span class="editdelcom"> 
-        <a href="edit_comment.php?id=<?= $comm['id']; ?>">Edit comment</a> | 
-    <a href="delete_comment.php?id=<?= $comm['id']; ?>">Delete comment</a>
-    
-    </span>
-        
-    <?php endif; ?>  
-        
-        </div> 
-    
-    <?php endif; ?> 
-        
+
+    <input type="submit" name="submit" value="Leave a comment" onclick="window.location = 'add_comment.php?id=<?= $post[ 'id' ]; ?>';"> <br><br>
+
+    <!--<a href="add_comment.php?id" class="commentslink"> Leave a comment: </a><br><br> -->
+
+
+    <?php foreach ( $comments as $comm ): ?>  
+
+        <?php if ( $comm[ 'post_id' ] == $post[ 'id' ] ): ?> 
+            <div class="sidebar-box-com"> 
+                <p><?= htmlentities( $comm[ 'comment' ] ); ?></p>  
+                <p><b>Written by: </b><i><?= htmlentities( $comm[ 'name' ] ); ?></i>, On: <?= $comm[ 'commentdate' ]; ?></p>
+                    <?php if ( $comm[ 'user_id' ] == $uid ): ?>  
+                        <p> 
+                            <span class="editdelcom">
+                            <a href="edit_comment.php?id=<?= $comm[ 'id' ]; ?>">Edit comment</a> | 
+                            <a href="delete_comment.php?id=<?= $comm[ 'id' ]; ?>">Delete comment</a>
+                            </span>
+                        </p>
+
+                    <?php endif; ?>  
+
+            </div> 
+
+        <?php endif; ?> 
+
     <?php endforeach; ?> 
-    
-      <?php include'tpl/footer.php'; ?>
-    
-    </div>
-    
+
+    <?php include'tpl/footer.php'; ?>
+
+</div>
 
 
 
-  
+
